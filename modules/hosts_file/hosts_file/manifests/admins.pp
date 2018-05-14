2ce8e879cb11e7f77c3ab995e9fe7c8e11274c33
Lewis Taylor <lewis.taylor@freshfields.com>
2018-05-14 08:29:53 +0000

    Added Examples file and Mod File.

---
+++ modules/hosts_file/hosts_file/manifests/admins.pp
@@ -0,0 +1,20 @@
+class system_users::admins {
+  group {'staff':
+  ensure   => present,
+  }
+  if $facts['kernel'] == 'windows'{
+  user {'admin':
+  groups   => 'staff',
+  }
+  }
+  else {
+  package {'csh':
+  ensure   => latest,
+  }
+  user {'admin':
+  groups   => 'staff',
+  shell    => '/bin/csh/',
+  require  => Package['csh']
+  }
+}
+}
